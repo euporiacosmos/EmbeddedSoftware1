0d59a89cdfd361488e777a9653ec78db6d5e56e9
euporiacosmos <euporiacosmos@gmail.com>
2023-05-21 09:37:55 +0000

    update project 2

--- pj02_flask_gpio_LEDControl_PushButtonMonitoring_test.py
+++ pj02_flask_gpio_LEDControl_PushButtonMonitoring_test.py
@@ -1,57 +1,24 @@
-from flask import Flask, render_template_string
-import RPi.GPIO as GPIO
+from flask import Flask, render_template, request, Response
+from pj02_flask_gpio_html_gpioControl import *
 
-GPIO.setmode(GPIO.BCM)
-BUTTON = 4 # constant is better than variable this case
-LED = 18
-ledStates = 0
-GPIO.setup(BUTTON, GPIO.IN)
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, False)
-
-app = Flask(__name__)
-
-def updateLed():
-    GPIO.output(LED, ledStates)
-
-control_page = '''
-    <script>
-    function changed(id)
-    {
-        window.location.href='/' + id;
-    }
-    </script>
-    <h1>GPIO Monitor-PushButton/Control-LED</h1>
-    <h2>Button State
-    {% if btnState %}
-    =Up
-    {% else %}
-    =Down
-    {% endif %}
-    </h2>
-    <input type="button" onClick="changed({[1]})" value='LED on'>
-    <input type="button" onClick="changed({[0]})" value='LED off'>
-    <meta http-equiv="refresh" content="1">
-    '''
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
-def index(led='n'): # GET REQUEST
-    global ledStates
+def do_route():
+    valPushButton = readButton()
+    return render_template("pj02_flask_gpio_html_index.html", btnState=valPushButton)
 
-    print('led=', led)
+#control rccar
+@app.route('/led', methods=['POST'])
+def control_led():
+    command=request.form.get('command')
+    #print(command)
+    if command == "rledON":
+        redOn()
+    elif command == "rledOFF":
+        redOff()
+    return ''
 
-    if led != 'n' and led != 'favicon.ico':
-        led = int(led)
-        if(led):
-            ledStates = True
-        else:
-            ledStates = False
-
-        print('ledStates=', ledStates)
-        GPIO.output(LED, ledStates)
-
-    valPushButton = GPIO.input(BUTTON)
-    return render_template_string(control_page, btnState=valPushButton)
 
 if __name__ == '__main__': # Program start from here
     app.run(host='192.168.0.2', port=8080)
